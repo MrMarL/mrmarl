f2a816474e491dbd57a12b373dbf173acbd1e989
Oh hi Mark! <61845375+MrMarL@users.noreply.github.com>
2020-10-11 15:55:17 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<style>
+body { background: url(https://mfiles.alphacoders.com/659/659130.png); }
+</style>
+<html>
+   <head>
+       <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+       <br>
+       <a href="http://b91125f1.beget.tech/Laba.php">ЛАБА</a>
+       <br>
+      <link rel="stylesheet" href="Style.css">
+   </head>
+   <body>
+      Конец сайта(
+   </body>
+</html>
